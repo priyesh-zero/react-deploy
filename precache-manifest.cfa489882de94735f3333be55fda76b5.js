3677b707ff127051eab9d3e00509f02068a5f8a7
priyesh-zero <priyesh.zero@gmail.com>
2020-09-29 08:30:08 +0000

    Updates

---
+++ precache-manifest.cfa489882de94735f3333be55fda76b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae406b8daf6ae928f76011e6a1894eec",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "89a31c83b18ecbec48cd",
+    "url": "/react-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b7e77ef4bb4cd171b476",
+    "url": "/react-deploy/static/js/2.39aa39c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.39aa39c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a31c83b18ecbec48cd",
+    "url": "/react-deploy/static/js/main.80fe7b71.chunk.js"
+  },
+  {
+    "revision": "d48a7c0e6e2704d58894",
+    "url": "/react-deploy/static/js/runtime-main.9a604473.js"
+  }
+]);
